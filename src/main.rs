648c6b0f6fe52bb2cad7a9d4fe5fbd81702c3c47
dworpss <dworpell@andrew.cmu.edu>
2019-04-07 17:46:40 -0400

    Made a change that somehow allowed for Traits to be used in sub directories

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod security_services;
-
+mod Traits;
 use crate::security_services::access_points::TraitSecureDataService;
-
+use crate::Traits::Tester;
 //If you try to include the line below, you get a compilation error proving that the trait is private therefore all the methods are private within it asewell.
 //use crate::Service::PrivateSDS;
 use std::io;
@@ -10,5 +10,6 @@ fn main() {
   let (sds,ssme) = security_services::initialize();
   println!("Compiled ok {}", sds.get_string());
   println!("Compiled ok {}", ssme.get_string());
+  println!("{}",sds.getme());
   //println!("The secret is {}", SSME.Secret());
 }
--- src/security_services/data_service.rs
+++ src/security_services/data_service.rs
@@ -1,13 +1,16 @@
 use super::access_points::TraitSecureDataService;
-
+use super::super::Traits;
 pub struct SecureDataService {
   s:String,
 }
-
 trait PrivateSDS {
   fn get_secret(&self) -> String;
 }
-
+impl Traits::Tester for SecureDataService {
+  fn getme(&self) -> String{
+    String::from("HE")
+  }
+}
 impl TraitSecureDataService for SecureDataService {
   fn get_string(&self) -> String {
     String::from("Greetings!")
