125eb1d4559954cbb2ee235b117d5467f82e7399
ChrisHensler <18620213+ChrisHensler@users.noreply.github.com>
2019-04-07 23:25:46 -0400

    attempting to add data structures

--- src/DataTypes/mod.rs
+++ src/DataTypes/mod.rs
@@ -22,3 +22,72 @@ pub enum ResultCode {
   IncorrectReqCertChainLengthForSecProfile,
   IncorrectReqCertChainLengthForImpl
 }
+
+enum HashAlgo {
+  sha256
+}
+enum SignerIdType {
+  Certificate,
+  Digest,
+  SelfSigned
+}
+enum ECPointFormat {
+  Compressed,
+  Uncompressed
+}
+enum FastVerificationOptions {
+  Compressed,
+  Uncompressed,
+  No
+}
+
+pub struct SignerIdentifier {
+  //todo: certificate
+  digest[char; 8],
+  self_signed=bool,
+}
+
+pub struct HeaderInfo {
+  psid: u64,
+  generation_time: u64,
+  expiry_time: u64,
+  //generationLocation
+}
+
+pub struct HashedData {
+  sha256HashedData: [char; 32]
+}
+
+pub struct SignedDataPayload {
+  data: Ieee1609Dot2DataRaw,  //needed to avoid circular definition
+  extDataHash: HashedData
+}
+
+pub struct ToBeSignedData {
+  payload: SignedDataPayload,
+  header_info: HeaderInfo
+}
+
+pub struct SignedData  {
+  hash_id: HashAlgo,
+  tbs_data: ToBeSignedData,
+  signer: SignerIdentifier,
+  signature: Signature
+}
+
+pub struct EncryptedData  {
+  //TODO
+}
+
+pub struct Ieee1609Dot2Content  {
+  unsecured_data: String,
+  signed_data: SignedData,
+  encrypted_data: EncryptedData,
+  signed_cert_data: String
+}
+
+
+pub struct Ieee1609Dot2DataRaw  {
+  protocol_version: u8,
+  content: String
+}
--- src/security_services/access_points.rs
+++ src/security_services/access_points.rs
@@ -1,4 +1,75 @@
+use std::time::SystemTime;
+use super::super::DataTypes;
+
+
+enum ResultCode_SecSignedData {
+  Success,
+  IncorrectInput,
+  NoCertificateProvided,
+  NoPublicKeyProvided,
+  NotEnoughInfo,
+  NoTrustAnchor,
+  ChainTooLong,
+  NotCryptoValid,
+  UnkownCryptoValid,
+  InconsistentChainPermissions,
+  Revoked,
+  Dubious,
+  UnsupportedCriticalInfoFields,
+  InvalidEncoding,
+  CurrTimeBeforeCertValid,
+  CurrTimeAfterCertValid,
+  ExpireBeforeCertValid,
+  ExpireAfterCertValid,
+  InvalidGenLocation,
+  InconsistentCertPermissions,
+  IncorrectReqCertChainLengthForSecProfile,
+  IncorrectReqCertChainLengthForImpl,
+}
+
+
 pub trait TraitSecureDataService {
   fn get_string(&self) -> String;
   fn secret(&self) -> String;
+
+  /*
+  Sec-SignedData.request (
+    Cryptomaterial Handle,
+    Data (optional),
+    Data Type (optional),
+    External Data Hash (optional),
+    External Data Hash Algorithm (optional),
+    PSID,
+    Set Generation Time,
+    Set Generation Location,
+    Expiry Time (optional),
+    Signer Identifier Type,
+    Signer Identifier Certificate Chain Length (optional),
+    Maximum Certificate Chain Length (optional),
+    Sign With Fast Verification,
+    EC Point Format,
+    Use Peer-to-Peer Cert Distribution,
+    SDEE ID (optional)
+  )
+  */
+
+  fn SecSignedDataRaw(&self,
+    cryptomaterial_handle: u64,
+    data: String,
+    ext_data_hash: String,
+    ext_data_hash_algo: String,
+    psid: u64,
+    set_generation_time: bool,
+    set_generation_location: bool,
+    expiry_time: SystemTime,
+    signer_id_type: DataTypes::SignerIdType,
+    signer_id_cert_chain_len: u64,
+    max_cert_chain_len: u8,
+    fast_verification: DataTypes::FastVerificationOptions,
+    ec_point_format: DataTypes::ECPointFormat,
+    use_p2pcd: bool,
+    sdee_id: u64
+  ) -> (
+    ResultCode_SecSignedData, String //todo: ResultCode and IEEEData
+  );
 }
