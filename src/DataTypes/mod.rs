ec5bb09b27944ddb14ff5cda650ae13d238fb867
dworpss <dworpell@andrew.cmu.edu>
2019-04-07 23:22:32 -0400

    Finished Sec Result codes

--- src/DataTypes/mod.rs
+++ src/DataTypes/mod.rs
@@ -1,4 +1,4 @@
-pub enum ResultCode {
+pub enum ResultCode_SecSignedData {
   Success,
   IncorrectInput,
   NoCertificateProvided,
@@ -20,5 +20,45 @@ pub enum ResultCode {
   InvalidGenLocation,
   InconsistentCertPermissions,
   IncorrectReqCertChainLengthForSecProfile,
-  IncorrectReqCertChainLengthForImpl
+  IncorrectReqCertChainLengthForImpl,
 }
+pub enum ResultCode_EncryptedData {
+  Success, IncorrectInputs, FailOnSomeCertificates, FailOnAllCertificates,
+}
+pub enum ResultCode_SecureDataPreProcess {
+  Success, InvalidInput, UnknownCertificate, InconsistentPSID,
+}
+pub enum SPDUParsing {
+  InvalidInput, UnsupportedCriticalInfoField, CertificateNotFound, GenerationTimeNotAvailable, GenerationLocationNotAvailable,
+}
+pub enum SPDUCertificateChain {
+  NotEnoughInfoToConstructChain,ChainEndedAtUntrustedRoot, ChainWasTooLongForImplementation, CertificateRevoked, OverdueCRL, InconsistentExpiryTimes,InconsistentStartTimes,InconsistentChainPermissions,
+}
+pub enum SPDUConsistency {
+  FutureCertificateAtGenerationTime, ExpiredCertificateAtGenerationTime, ExpiryDateTooEarly,ExpiryDateTooLate, GenerationLocationOutsideValidityRegion, NoGenerationLocation, UnauthorizedPSID,
+}
+pub enum SPDUCrypto {
+  VerificationFailure,
+}
+pub enum SPDUInternalConsistency {
+  ExpiryTimeBeforeGenerationTime, ExtDataHashDoesntMatch, NoExtDataHashProvided, NoExtDataHashPresent,
+}
+pub enum SPDULocalConsistency {
+  PSIDsDontMatch, ChainWasTooLongForSDEE,
+}
+pub enum SPDURelevance {
+  GenerationTimeTooFarInPast, GenerationTimeTooFarInFuture, ExpiryTimeInPast, GenerationLocationTooDistant, ReplayedSPDU, CertificateExpired,
+}
+pub enum ResultCode_SecSignedDataVerification {
+  SPDUParsing(SPDUParsing),
+  SPDUCertificateChain(SPDUCertificateChain),
+  SPDUConsistency(SPDUConsistency),
+  SPDUCrypto(SPDUCrypto),
+  SPDUInternalConsistency(SPDUInternalConsistency),
+  SPDULocalConsistency(SPDULocalConsistency),
+  SPDURelevance(SPDURelevance),
+}
+pub enum ResultCode_SecEncryptedDataDecryption {
+  Success,NoDecryptionKeyAvailable,UnsupportedCriticalInformationField,CouldntDecryptKey,CouldntDecryptData,InvalidFormForPlainText,
+}
+
